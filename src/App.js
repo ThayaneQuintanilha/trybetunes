af4d6319dcf552f09b24b44563f4b36d1b21e117
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-30 14:54:32 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes </p>);
+    return (
+
+      <BrowserRouter>
+        <Login />
+        <Search />
+        <Album />
+        <Favorites />
+        <Profile />
+        <ProfileEdit />
+        <NotFound />
+      </BrowserRouter>
+    );
   }
 }
 

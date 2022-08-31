acf77f7f3d51976fffc3f87dfa5268a32fc3a111
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-31 17:09:37 -0300

    Requisito 6 feito finalmente...

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 export default class search extends Component {
   state = {
     buttonDisabled: true,
     inputValue: '',
+    showInput: false,
+    albumArray: [],
+    saveInputValue: '',
   };
 
-  handleChange = ({ target }) => {
+  handleChange = async ({ target }) => {
     const { name, value } = target;
     this.setState({ [name]: value });
-
     const number = 2;
     if (value.length >= number) {
       this.setState({ buttonDisabled: false });
@@ -19,14 +24,33 @@ export default class search extends Component {
     }
   };
 
+  handleClick = async () => {
+    this.setState({ showInput: true });
+
+    const { inputValue } = this.state;
+    const api = await searchAlbumsAPI(inputValue);
+
+    this.setState({
+      inputValue: '',
+      saveInputValue: inputValue,
+      albumArray: api,
+      showInput: false,
+    });
+  };
+
   render() {
-    const { inputValue, buttonDisabled } = this.state;
+    const { inputValue,
+      buttonDisabled,
+      showInput,
+      saveInputValue, albumArray } = this.state;
     return (
       <div data-testid="page-search">
-        search
-        <Header />
-        <form>
+        <div>
+          search
+          <Header />
+
           <label htmlFor="label">
+
             <input
               onChange={ this.handleChange }
               data-testid="search-artist-input"
@@ -39,13 +63,36 @@ export default class search extends Component {
               type="button"
               name="inputValue"
               disabled={ buttonDisabled }
+              onClick={ this.handleClick }
               data-testid="search-artist-button"
             >
               Procurar
 
             </button>
           </label>
-        </form>
+        </div>
+
+        {showInput && <Loading />}
+        <h1>{`Resultado de álbuns de: ${saveInputValue}`}</h1>
+        {albumArray.length === 0 ? <p>Nenhum álbum foi encontrado</p>
+          : (
+            <div>
+              {albumArray.map((album) => {
+                const { collectionId, collectionName, artworkUrl100 } = album;
+                return (
+                  <div key={ collectionName }>
+                    <img src={ artworkUrl100 } alt={ collectionName } />
+                    <Link
+                      to={ `/album/${collectionId}` }
+                      data-testid={ `link-to-album-${collectionId}` }
+                    >
+                      {collectionName}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          )}
       </div>
     );
   }
